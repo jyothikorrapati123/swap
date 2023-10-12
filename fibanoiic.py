eef02f11470b4187f5bdf6ca26f05ccfde99e2f0
jyothikorrapati123 <144519938+jyothikorrapati123@users.noreply.github.com>
2023-10-12 21:22:19 +0530

    Add files via upload

---
+++ fibanoiic.py
@@ -0,0 +1,12 @@
+
+
+num = int(input("enter a number:"))
+
+a=1
+b=0
+i=1
+while i <=num:
+    print(b)
+    a=a+b
+    b=a-b
+    i+=1
---
+++ swap.py
@@ -0,0 +1,16 @@
+#swap 2 numbers
+
+a=1
+b=2
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+#swap 2 numbers with temp
+a=2
+b=1
+temp=a
+a=b
+b=temp
+print(a,b)
